697c4a8abeaef00d0c8049d4f4bda04661ef4c39
Jordan White <jwhite34@uw.edu>
2023-08-17 20:13:45 -0700

    gave video_processing.py the ability to play a video

--- src/video_processing.py
+++ src/video_processing.py
@@ -1,6 +1,35 @@
 import cv2
 
+video_path = '../fire_dataset/fire_videos/fire.mp4'
+
 
 def read_video(video_path):
-    # Code to read video
-    ...
+    # Create a VideoCapture object
+    video = cv2.VideoCapture(video_path)
+    if not video.isOpened():
+        print("Error: video could not be opened")
+    else:
+        # Loop through video frames
+        while True:
+            # Reading next frame
+            ret, frame = video.read()
+            if not ret:
+                break  # Breaking loop if end of video
+
+            cv2.imshow('Video', frame)  # Displaying frame
+
+            if cv2.waitKey(30) and 0xFF == ord('q'):
+                break
+
+        # Releasing video object, closing windows
+        video.release()
+        cv2.destroyAllWindows()
+
+
+def main():
+    # Load and preprocess the data
+    read_video(video_path)
+
+
+if __name__ == "__main__":
+    main()
