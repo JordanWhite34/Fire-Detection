1410745b36d3604bd279a0cbc95362aa22961f98
Jordan White <jwhite34@uw.edu>
2023-08-22 20:53:27 -0700

    added code for finding contours and extracting ROIs

--- src/color_detection.py
+++ src/color_detection.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+# Something that would be cool to add later would be a box where the color detection is and
+# have it change colors when fire is detected there, like from green to red or something
+
 
 # Convert the input image to the HSV color space
 def convert_to_hsv(image):
@@ -10,3 +13,15 @@ def convert_to_hsv(image):
 # Create a binary mask where the pixels within the specified color range are set to 1, others set to 0
 def create_mask(hsv_image, lower_bound, upper_bound):
     return cv2.inRange(hsv_image, lower_bound, upper_bound)
+
+
+# Find the contours/boundaries of connected components in the binary mask
+def find_contours(mask):
+    return cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+
+# Extract regions of interest (ROIs) from the original image using the contours
+def extract_rois(frame, contours):
+    rois = [cv2.boundingRect(contour) for contour in contours]
+    return [frame[y:y+h, x:x+w] for (x, y, w, h) in rois]
+
